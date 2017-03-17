e16bf3531b36fc40bbab346a1b409347353a74a6
santiago <santivaz852@hotmail.com>
2017-03-17 13:25:32 -0500

    nuevo

---
+++ zapatosApp/config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  resources :users
+    
+    root 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
